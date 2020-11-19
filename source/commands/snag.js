3a448afebefaca90018ecea22cb20ed1866997aa
PapaSnags <mail@papa-snags.com>
2020-11-19 11:22:29 +1100

    update

---
+++ source/commands/snag.js
@@ -0,0 +1,10 @@
+const discord = require ("discord.js");
+
+exports.run = (client, message, args) =>{
+        //Amount of images
+        number = 33;
+        //Randomly selects and image
+        imageNumber = Math.floor (Math.random() * (number -1 + 1)) + 1;
+        //Sets the folder location of the images and name must be nammed "1.jpg" / "2.jpg" and so on through to max amount of images you like 
+        message.channel.send ({files: ["./images/normal/" + imageNumber + ".jpg"]})
+}
