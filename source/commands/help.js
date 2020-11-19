1ca2805174f03a0c9b4d9a93213583ddbcda3ed4
PapaSnags <mail@papa-snags.com>
2020-11-19 23:10:37 +1100

    Update help.js

--- source/commands/help.js
+++ source/commands/help.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) =>{
     .setURL("https://github.com/PapaSnags/Snags-Supplier")
     .addFields(
         {name: "Snag", value: "Sends a normal image of a Snag (Sausage). \n **Usage:** s!snag \n"},
-        {name: "Cursed Snag", value: "Sends a cursed image of a Snag (Sausage). (Sorted by created date) \n **Usage:** s!cursedsnag \n"},
+        {name: "Cursed Snag", value: "Sends a cursed image of a Snag (Sausage). (Sorted by created date) \n **Usage:** s!cursed \n"},
         {name: "Help", value: "Sends This Help Message \n **Usage:** s!help\n"}
         )
     .setFooter("Snags Supplier Made by PapaSnags#1555", "https://cdn.discordapp.com/app-icons/602313830474842115/ebb9f69ded9fbaa5abbd568e50ede220.png?size=64")
