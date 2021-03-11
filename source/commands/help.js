4a277a4d2d305f357333fedcc0040f24894589e9
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2021-03-11 12:14:28 +1100

    Updated footer

--- source/commands/help.js
+++ source/commands/help.js
@@ -4,13 +4,13 @@ exports.run = (client, message, args) =>{
     const help = new discord.MessageEmbed()
     .setColor('#b434eb')
     .setTitle('Snags Supplier - Help')
-    .setURL("https://github.com/PapaSnags/Snags-Supplier")
+    .setURL("https://github.com/LachlanDev/Snags-Supplier")
     .addFields(
         {name: "Snag", value: "Sends a normal image of a Snag (Sausage). \n **Usage:** s!snag \n"},
         {name: "Cursed Snag", value: "Sends a cursed image of a Snag (Sausage). (Sorted by created date) \n **Usage:** s!cursed \n"},
         {name: "Help", value: "Sends This Help Message \n **Usage:** s!help\n"}
         )
-    .setFooter("Snags Supplier Made by PapaSnags#1555", "https://cdn.discordapp.com/app-icons/602313830474842115/ebb9f69ded9fbaa5abbd568e50ede220.png?size=64")
+    .setFooter("Snags Supplier Made by LachlanDev#8014", "https://cdn.discordapp.com/app-icons/602313830474842115/ebb9f69ded9fbaa5abbd568e50ede220.png?size=64")
     message.channel.send({embed: help })
 
 };
